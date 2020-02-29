13891b7db09a66b2f00518274c7be3b517b19c33
elbanan <elbanan@users.noreply.github.com>
2020-02-29 13:31:24 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,11 +1,21 @@
 from setuptools import setup
 
-setup(name='radtorch',
+setup(
+      name='radtorch',
       version='0.1.0',
       description='RADTorch, The Radiology Machine Learning Tool Kit',
-      url='https://github.com/radtorch/radtorch',
+      url='https://radtorch.github.io/radtorch/',
       author='Mohamed Elbanan, MD',
+      author_email = "https://www.linkedin.com/in/mohamedelbanan/",
       license='MIT',
       packages=['radtorch'],
       install_requires=['torch', 'torchvision', 'numpy', 'pandas', 'pydicom', 'matplotlib', 'pillow', 'tqdm', 'sklearn', 'pathlib'],
-      zip_safe=False)
+      zip_safe=False,
+      classifiers=[
+      "Development Status :: 4 - Beta",
+      "License :: OSI Approved :: MIT License",
+      "Natural Language :: English",
+      "Programming Language :: Python :: 3 :: Only",
+      "Topic :: Software Development :: Libraries :: Python Modules",
+      ]
+      )
