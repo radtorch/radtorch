49e92567cb38886f5d7a330a70b14d520d04344b
elbanan <elbanan@users.noreply.github.com>
2020-03-25 14:18:23 -0400

    Update pipeline.py

--- radtorch/pipeline.py
+++ radtorch/pipeline.py
@@ -860,5 +860,5 @@ class Compare_Image_Classifier():
         torch.cuda.empty_cache()
 
 
-    def metrics(self):
-        return show_multiple_metrics(self.classifiers)
+    def metrics(self,  fig_size=(800,600)):
+        return show_multiple_metrics(self.classifiers,  fig_size=fig_size)
