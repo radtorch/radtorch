732998f82a048b14f03bed23b9c0df07257e7406
elbanan <elbanan@users.noreply.github.com>
2020-03-28 13:46:09 -0400

    Update pipeline.py

--- radtorch/pipeline.py
+++ radtorch/pipeline.py
@@ -941,7 +941,7 @@ class Compare_Image_Classifier():
         self.auc_list = show_roc(self.classifiers, fig_size=fig_size)
         self.best_model_auc = max(self.auc_list)
         self.best_model_index = (self.auc_list.index(self.best_model_auc))
-        self.best_classifier = self.classifiers.index(self.best_model_index)
+        self.best_classifier = self.classifiers[self.best_model_index]
 
     def best(self, path=None, export_classifier=False, export_model=False):
         try:
