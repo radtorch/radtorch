f5073c9bd56faee63a66b5cd5eb5c51ddd519cd4
elbanan <elbanan@users.noreply.github.com>
2020-03-29 03:11:57 -0400

    upadte dependencies

--- radtorch/dicomutils.py
+++ radtorch/dicomutils.py
@@ -1,22 +1,3 @@
-import torch, torchvision, datetime, time, pickle, pydicom, os
-import torchvision.models as models
-import torch.nn as nn
-import torch.optim as optim
-import torch.nn.functional as F
-import torchvision.datasets as datasets
-import matplotlib.pyplot as plt
-import numpy as np
-import pandas as pd
-
-
-from sklearn import metrics
-from tqdm import tqdm_notebook as tqdm
-from torch.utils.data.dataset import Dataset
-from torchvision import transforms
-from PIL import Image
-from pathlib import Path
-
-
 
 
 def window_dicom(filepath, level, width):
@@ -40,7 +21,7 @@ def dicom_to_narray(filepath, mode='RAW', wl=None):
     """
     .. include:: ./documentation/docs/dicomutils.md##dicom_to_narray
     """
-    
+
     if mode == 'RAW':
         ds = pydicom.read_file(filepath)
         img = ds.pixel_array
