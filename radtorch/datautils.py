69f4a92eba95000e2e70348da0cee87cd0a0c556
kareemelfatairy <kareemfa@hotmail.com>
2020-03-30 22:39:14 -0400

    Update datautils.py

--- radtorch/datautils.py
+++ radtorch/datautils.py
@@ -136,9 +136,6 @@ def list_of_files(root):
 
 
 
-
-
-
 def path_to_class(filepath):
     """
     .. include:: ./documentation/docs/datautils.md##path_to_class
@@ -170,13 +167,13 @@ def class_to_idx(classes):
     return class_to_idx
 
 def datatable_from_filepath(*filelist,classes:list): #KareemElFatairy
-    ''' purpose: Create dataframe of file pathes and labels extracted from supplied folders.
+    """ purpose: Create dataframe of file pathes and labels extracted from supplied folders.
         Argument:
         *filelist: returns list of paths.
         classes: a list of desired classes as seen in file name.
-    '''
+    """
     file_lists = map(list_of_files,filelist)  #get a list of files from folders
-    data={'image_path':[],'image_label':[]}
+    data={'IMAGE_PATH':[],'IMAGE_LABEL':[]}
     for file_list in file_lists:
       for file_path in file_list: #create lists of files with the specified label and append to the dictionary
         for item in classes:
