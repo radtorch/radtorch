909d031af9d53c52965ac3f1f2f9c33dec974ebf
elbanan <elbanan@users.noreply.github.com>
2020-04-04 13:33:28 -0400

    Update datautils.py

--- radtorch/datautils.py
+++ radtorch/datautils.py
@@ -225,6 +225,7 @@ class Dataset_from_table(RADTorch_Dataset):
     def __init__(self,*args, **kwargs):
         for k,v in kwargs.items():
             setattr(self, k, v)
+        super(Dataset_from_table, self).__init__()
 
         if self.table==None:
             raise TypeError('Error! No label table was selected. Please check.')
@@ -262,7 +263,7 @@ class Dataset_from_folder(RADTorch_Dataset):
     def __init__(self,*args, **kwargs):
         for k,v in kwargs.items():
             setattr(self, k, v)
-
+        super(Dataset_from_folder, self).__init__()
         self.classes, self.class_to_idx = root_to_class(self.data_directory)
         self.all_files = list_of_files(self.data_directory)
         if self.is_dicom:
