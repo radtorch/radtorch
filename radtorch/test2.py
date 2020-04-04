a36f184251bca0b104ed9b397801416b55f19f2f
elbanan <elbanan@users.noreply.github.com>
2020-04-04 19:05:05 -0400

    Update test2.py

--- radtorch/test2.py
+++ radtorch/test2.py
@@ -75,8 +75,7 @@ class Pipeline():
 class Image_Classification(Pipeline):
     def __init__(self, **kwargs):
         super(Image_Classification, self).__init__(**kwargs, DEFAULT_SETTINGS=IMAGE_CLASSIFICATION_PIPELINE_SETTINGS)
-
-        if isinstance(self.table, pd.DataFrame) : self.dataset=Dataset_from_table(kwargs)
-        else: self.dataset=Dataset_from_folder(kwargs)
+        if isinstance(self.table, pd.DataFrame): self.dataset=Dataset_from_table(**kwargs)
+        else: self.dataset=Dataset_from_folder(**kwargs)
         self.num_output_classes = len(self.dataset.classes)
         self.dataloader = torch.utils.data.DataLoader(kwargs, dataset=self.dataset)
