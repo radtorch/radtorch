2ae4980d4c0ea2a8a2e153d33ae80948a1ab7d97
elbanan <elbanan@users.noreply.github.com>
2020-04-04 13:36:36 -0400

    Update datautils.py

--- radtorch/datautils.py
+++ radtorch/datautils.py
@@ -223,7 +223,7 @@ class RADTorch_Dataset(Dataset):
 
 class Dataset_from_table(RADTorch_Dataset):
     def __init__(self,*args, **kwargs):
-        super(Dataset_from_table, self).__init__(**kwargs)
+        super(Dataset_from_table, self).__init__(kwargs)
         # for k,v in kwargs.items():
         #     setattr(self, k, v)
         if self.table==None:
@@ -260,7 +260,7 @@ class Dataset_from_table(RADTorch_Dataset):
 
 class Dataset_from_folder(RADTorch_Dataset):
     def __init__(self,*args, **kwargs):
-        super(Dataset_from_folder, self).__init__(**kwargs)
+        super(Dataset_from_folder, self).__init__(kwargs)
         #
         # for k,v in kwargs.items():
         #     setattr(self, k, v)
