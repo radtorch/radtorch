7104fe3d549dec154f371a3c73528e76877f93e6
elbanan <elbanan@users.noreply.github.com>
2020-04-05 21:23:48 -0400

    Update pipeline.py

--- radtorch/pipeline.py
+++ radtorch/pipeline.py
@@ -133,15 +133,11 @@ class Image_Classification(Pipeline):
         # Create train/valid/test datasets and dataloaders
         for k, v in self.dataset_dictionary.items():
             if self.fly: # Use 10% for quick fly testing
-                subset_indices=torch.utils.data.RandomSampler(data_source=v, replacement=True, num_samples=int(len(v)/10))
-                shuffle=False
-                if self.balance_class: setattr(self, k+'_dataset', v.balance())
-                else: setattr(self, k+'_dataset', v)
-                setattr(self, k+'_dataloader', torch.utils.data.DataLoader(dataset=self.__dict__[k+'_dataset'], sampler=SubsetRandomSampler(subset_indices), batch_size=self.batch_size, shuffle=False, num_workers=self.num_workers))
-            else:
-                if self.balance_class: setattr(self, k+'_dataset', v.balance())
-                else: setattr(self, k+'_dataset', v)
-                setattr(self, k+'_dataloader', torch.utils.data.DataLoader(dataset=self.__dict__[k+'_dataset'], batch_size=self.batch_size, shuffle=True, num_workers=self.num_workers))
+                samples = torch.utils.data.random_split(dataset=v, lengths=int(len(v)/10))
+                v = samples[0]
+            if self.balance_class: setattr(self, k+'_dataset', v.balance())
+            else: setattr(self, k+'_dataset', v)
+            setattr(self, k+'_dataloader', torch.utils.data.DataLoader(dataset=self.__dict__[k+'_dataset'], batch_size=self.batch_size, shuffle=True, num_workers=self.num_workers))
 
 
         # Create Training Model
