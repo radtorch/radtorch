5aefd249194b3ae701469a3b08350f13913e677d
elbanan <elbanan@users.noreply.github.com>
2020-04-06 15:34:19 -0400

    Update test.py

--- radtorch/test.py
+++ radtorch/test.py
@@ -60,8 +60,9 @@ class Classifier():
             if 'vgg' in self.model_arch or 'alexnet' in self.model_arch: self.model.classifier[6]=torch.nn.Sequential(torch.nn.Linear(in_features=self.in_features, out_features=self.output_classes, bias=True),torch.nn.LogSoftmax(dim=1))
             elif 'resnet' in self.model_arch: self.model.fc=torch.nn.Sequential(torch.nn.Linear(in_features=self.in_features, out_features=self.output_classes, bias=True),torch.nn.LogSoftmax(dim=1))
 
-    def __new__(self):
-        return self.model
+    def __new__(cls, **kwargs):
+        super(A, cls).__new__(cls)
+        return  self.model
 
 class Optimizer():
     def __init__(self, **kwargs):
