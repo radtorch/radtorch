ca1d6895c0c82b1198dd5dde6fda839ff7c6f5d0
Mohamed Elbanan <elbanan@users.noreply.github.com>
2020-04-08 16:20:27 -0400

    Update pipeline.py

--- radtorch/pipeline.py
+++ radtorch/pipeline.py
@@ -140,7 +140,7 @@ class Image_Classification(Pipeline):
 
 
         # Create Training Model
-        self.feature_extractor=Feature_Extractor(model_arch=self.model_arch, pre_trained=self.pre_trained)
+        self.feature_extractor=Feature_Extractor(model_arch=self.model_arch, pre_trained=self.pre_trained, freeze=self.freeze)
         self.train_model=Classifier(feature_extractor=self.feature_extractor, output_classes=self.num_output_classes, type=self.classifier_type, output_features=self.output_features)
         self.train_model=self.train_model.to(self.device)
 
@@ -226,7 +226,7 @@ class Feature_Extraction(Pipeline):
     def __init__(self, **kwargs):
         super(Feature_Extraction, self).__init__(**kwargs, DEFAULT_SETTINGS=FEATURE_EXTRACTION_PIPELINE_SETTINGS)
         self.classifiers=[self]
-        self.model=Feature_Extractor(model_arch=self.model_arch, pre_trained=self.pre_trained).model
+        self.model=Feature_Extractor(model_arch=self.model_arch, pre_trained=self.pre_trained, freeze=True).model
         # self.model=create_model(model_arch=self.model_arch,output_classes=self.num_output_classes,pre_trained=self.pre_trained,unfreeze_weights=self.unfreeze_weights, mode='feature_extraction')
 
     def num_features(self):
