5f96b0b904f3c3643ef1733f51044fb051b9b3f7
elbanan <elbanan@users.noreply.github.com>
2020-05-06 00:03:41 -0400

    update data and core

--- radtorch/core.py
+++ radtorch/core.py
@@ -16,6 +16,117 @@ from radtorch.general import *
 from radtorch.data import *
 
 
+class RADTorch_Dataset(Dataset):
+
+    def __init__(self, **kwargs): #defines the default parameters for dataset class.
+        for k,v in kwargs.items():
+            setattr(self, k, v)
+
+        for k, v in DEFAULT_DATASET_SETTINGS.items():
+            if k not in kwargs.keys():
+                setattr(self, k, v)
+
+    def __getitem__(self, index): #handles how to get an image of the dataset.
+        image_path=self.input_data.iloc[index][self.image_path_column]
+        if self.is_dicom:
+            image=dicom_to_narray(image_path, self.mode, self.wl)
+            image=Image.fromarray(image)
+        else:
+            image=Image.open(image_path).convert('RGB')
+
+        image=self.transformations(image)
+
+        label=self.input_data.iloc[index][self.image_label_column]
+        label_idx=[v for k, v in self.class_to_idx.items() if k == label][0]
+
+        return image, label_idx, image_path
+
+    def __len__(self): #returns number of images in dataset.
+        return len(self.dataset_files)
+
+    def info(self): #returns table of dataset information.
+        return show_dataset_info(self)
+
+    def classes(self): #returns list of classes in dataset.
+        return self.classes
+
+    def class_to_idx(self): #returns mapping of classes to class id (dictionary).
+        return self.class_to_idx
+
+    def parameters(self): #returns all the parameter names of the dataset.
+        return self.__dict__.keys()
+
+    def split(self, **kwargs): #splits dataset into train/valid/split, takes test_percent and valid_percent.
+        return split_dataset(dataset=self, **kwargs)
+
+    def balance(self, **kwargs): #solves class imbalance in dataset through over-sampling of classes with less images.
+        # return over_sample(dataset=self, **kwargs)
+        return balance_dataset(dataset=self, **kwargs)
+
+    def mean_std(self): #calculates mean and standard deviation of dataset.
+        self.mean, self.std= calculate_mean_std(torch.utils.data.DataLoader(dataset=self))
+        return tuple(self.mean.tolist()), tuple(self.std.tolist())
+
+    def normalize(self, **kwargs): #retruns a normalized dataset with either mean/std of the dataset or a user specified mean/std
+        if 'mean' in kwargs.keys() and 'std' in kwargs.keys():
+            mean=kwargs['mean']
+            std=kwargs['std']
+        else:
+            mean, std=self.mean_std()
+        normalized_dataset=copy.deepcopy(self)
+        normalized_dataset.transformations.transforms.append(transforms.Normalize(mean=mean, std=std))
+        return normalized_dataset
+
+
+class Dataset_from_table(RADTorch_Dataset):
+    def __init__(self, **kwargs):
+        super(Dataset_from_table, self).__init__(**kwargs)
+        if isinstance(self.table, pd.DataFrame):
+            self.input_data=self.table
+        elif self.table != None:
+            self.input_data=pd.read_csv(self.input_source)
+        else:
+            raise TypeError('Error! No label table was selected. Please check.')
+        if self.is_dicom: self.dataset_files=[x for x in (self.input_data[self.image_path_column].tolist()) if x.endswith('.dcm')]
+        else: self.dataset_files=[x for x in (self.input_data[self.image_path_column].tolist()) if x.endswith(IMG_EXTENSIONS)]
+        if self.multi_label == True:
+            self.classes=list(np.unique([item for t in self.input_data[self.image_label_column].to_numpy() for item in t]))
+            self.class_to_idx=class_to_idx(self.classes)
+            self.multi_label_idx=[]
+            for i, row in self.input_data.iterrows():
+                t=[]
+                for u in self.classes:
+                    if u in row[self.image_label_column]:
+                        t.append(1)
+                    else:
+                        t.append(0)
+                self.multi_label_idx.append(t)
+            self.input_data['MULTI_LABEL_IDX']=self.multi_label_idx
+        else:
+            self.classes= list(self.input_data[self.image_label_column].unique())
+            self.class_to_idx=class_to_idx(self.classes)
+        if len(self.dataset_files)==0:
+            print ('Error! No data files found in directory:', self.data_directory)
+
+        if len(self.classes)    ==0:
+            print ('Error! No classes extracted from directory:', self.data_directory)
+
+
+class Dataset_from_folder(RADTorch_Dataset):
+    def __init__(self, **kwargs):
+        super(Dataset_from_folder, self).__init__(**kwargs)
+        self.classes, self.class_to_idx=root_to_class(self.data_directory)
+        self.all_files=list_of_files(self.data_directory)
+        if self.is_dicom: self.dataset_files=[x for x in self.all_files  if x.endswith('.dcm')]
+        else: self.dataset_files=[x for x in self.all_files if x.endswith(IMG_EXTENSIONS)]
+        self.all_classes=[path_to_class(i) for i in self.dataset_files]
+        self.input_data=pd.DataFrame(list(zip(self.dataset_files, self.all_classes)), columns=[self.image_path_column, self.image_label_column])
+        if len(self.dataset_files)==0:
+            print ('Error! No data files found in directory:', self.data_directory)
+        if len(self.classes)==0:
+            print ('Error! No classes extracted from directory:', self.data_directory)
+
+
 class Data_Processor():
     '''
     kwargs: device, table, data_directory, is_dicom, mode, wl, normalize, balance_class, batch_size, num_workers, model_arch , custom_resize,
@@ -94,7 +205,7 @@ class Data_Processor():
         if self.type=='nn_classifier':
             self.train_dataset=Dataset_from_table(table=self.train_table, **self.train_dataset_kwargs)
             if self.balance_class:
-                self.train_dataset=self.train_dataset.balance()
+                self.train_dataset=self.train_dataset.balance(label_col=self.image_label_col, upsample=True)
             self.valid_dataset=Dataset_from_table(table=self.valid_table, **self.dataset_kwargs)
             self.train_dataloader=torch.utils.data.DataLoader(dataset=self.train_dataset, batch_size=self.batch_size, shuffle=True, num_workers=self.num_workers)
             self.valid_dataloader=torch.utils.data.DataLoader(dataset=self.valid_dataset, batch_size=self.batch_size, shuffle=True, num_workers=self.num_workers)
@@ -102,7 +213,7 @@ class Data_Processor():
         else:
             self.train_dataset=Dataset_from_table(table=self.temp_table,  **self.train_dataset_kwargs)
             if self.balance_class:
-                self.train_dataset=self.train_dataset.balance()
+                self.train_dataset=self.train_dataset.balance(label_col=self.image_label_col, upsample=True)
             self.train_dataloader=torch.utils.data.DataLoader(dataset=self.train_dataset, batch_size=self.batch_size, shuffle=True, num_workers=self.num_workers)
 
         self.test_dataset=Dataset_from_table(table=self.test_table, **self.dataset_kwargs)
@@ -192,14 +303,11 @@ class Feature_Extractor():
         elif self.model_arch=='alexnet': self.model=torchvision.models.alexnet(pretrained=self.pre_trained)
 
         if 'alexnet' in self.model_arch:
-            self.model.classifier=torch.nn.Sequential(
-                                                torch.nn.Linear(in_features=9216, out_features=4096, bias=True),
-                                                torch.nn.Identity())
+            self.model.classifier=torch.nn.Linear(in_features=9216, out_features=4096, bias=True)
         elif 'vgg' in self.model_arch:
-            self.model.classifier=torch.nn.Sequential(
-                                                torch.nn.Linear(in_features=25088, out_features=4096, bias=True),
-                                                torch.nn.Identity())
-        elif 'resnet' in self.model_arch: self.model.fc=torch.nn.Identity()
+            self.model.classifier=torch.nn.Linear(in_features=25088, out_features=4096, bias=True)
+        elif 'resnet' in self.model_arch:
+            self.model.fc=torch.nn.Identity()
 
         if self.unfreeze:
             for param in self.model.parameters():
--- radtorch/data.py
+++ radtorch/data.py
@@ -19,122 +19,6 @@ from radtorch.vis import *
 from radtorch.settings import *
 
 
-
-class RADTorch_Dataset(Dataset):
-    def __init__(self, **kwargs): #defines the default parameters for dataset class.
-        for k,v in kwargs.items():
-            setattr(self, k, v)
-
-        for k, v in DEFAULT_DATASET_SETTINGS.items():
-            if k not in kwargs.keys():
-                setattr(self, k, v)
-
-    def __getitem__(self, index): #handles how to get an image of the dataset.
-        image_path=self.input_data.iloc[index][self.image_path_column]
-        if self.is_dicom:
-            image=dicom_to_narray(image_path, self.mode, self.wl)
-            image=Image.fromarray(image)
-
-        else:
-            image=Image.open(image_path).convert('RGB')
-
-        image=self.transformations(image)
-
-        if self.multi_label == True:
-            label=self.input_data.iloc[index][self.image_label_column]
-            label_idx=self.input_data.iloc[index]['MULTI_LABEL_IDX']
-
-        else:
-            label=self.input_data.iloc[index][self.image_label_column]
-            label_idx=[v for k, v in self.class_to_idx.items() if k == label][0]
-
-        return image, label_idx, image_path
-
-    def __len__(self): #returns number of images in dataset.
-        return len(self.dataset_files)
-
-    def info(self): #returns table of dataset information.
-        return show_dataset_info(self)
-
-    def classes(self): #returns list of classes in dataset.
-        return self.classes
-
-    def class_to_idx(self): #returns mapping of classes to class id (dictionary).
-        return self.class_to_idx
-
-    def parameters(self): #returns all the parameter names of the dataset.
-        return self.__dict__.keys()
-
-    def split(self, **kwargs): #splits dataset into train/valid/split, takes test_percent and valid_percent.
-        return split_dataset(dataset=self, **kwargs)
-
-    def balance(self, **kwargs): #solves class imbalance in dataset through over-sampling of classes with less images.
-        return over_sample(dataset=self, **kwargs)
-
-    def mean_std(self): #calculates mean and standard deviation of dataset.
-        self.mean, self.std= calculate_mean_std(torch.utils.data.DataLoader(dataset=self))
-        return tuple(self.mean.tolist()), tuple(self.std.tolist())
-
-    def normalize(self, **kwargs): #retruns a normalized dataset with either mean/std of the dataset or a user specified mean/std
-        if 'mean' in kwargs.keys() and 'std' in kwargs.keys():
-            mean=kwargs['mean']
-            std=kwargs['std']
-        else:
-            mean, std=self.mean_std()
-        normalized_dataset=copy.deepcopy(self)
-        normalized_dataset.transformations.transforms.append(transforms.Normalize(mean=mean, std=std))
-        return normalized_dataset
-
-
-class Dataset_from_table(RADTorch_Dataset):
-    def __init__(self, **kwargs):
-        super(Dataset_from_table, self).__init__(**kwargs)
-        if isinstance(self.table, pd.DataFrame):
-            self.input_data=self.table
-        elif self.table != None:
-            self.input_data=pd.read_csv(self.input_source)
-        else:
-            raise TypeError('Error! No label table was selected. Please check.')
-        if self.is_dicom: self.dataset_files=[x for x in (self.input_data[self.image_path_column].tolist()) if x.endswith('.dcm')]
-        else: self.dataset_files=[x for x in (self.input_data[self.image_path_column].tolist()) if x.endswith(IMG_EXTENSIONS)]
-        if self.multi_label == True:
-            self.classes=list(np.unique([item for t in self.input_data[self.image_label_column].to_numpy() for item in t]))
-            self.class_to_idx=class_to_idx(self.classes)
-            self.multi_label_idx=[]
-            for i, row in self.input_data.iterrows():
-                t=[]
-                for u in self.classes:
-                    if u in row[self.image_label_column]:
-                        t.append(1)
-                    else:
-                        t.append(0)
-                self.multi_label_idx.append(t)
-            self.input_data['MULTI_LABEL_IDX']=self.multi_label_idx
-        else:
-            self.classes= list(self.input_data[self.image_label_column].unique())
-            self.class_to_idx=class_to_idx(self.classes)
-        if len(self.dataset_files)==0:
-            print ('Error! No data files found in directory:', self.data_directory)
-
-        if len(self.classes)    ==0:
-            print ('Error! No classes extracted from directory:', self.data_directory)
-
-
-class Dataset_from_folder(RADTorch_Dataset):
-    def __init__(self, **kwargs):
-        super(Dataset_from_folder, self).__init__(**kwargs)
-        self.classes, self.class_to_idx=root_to_class(self.data_directory)
-        self.all_files=list_of_files(self.data_directory)
-        if self.is_dicom: self.dataset_files=[x for x in self.all_files  if x.endswith('.dcm')]
-        else: self.dataset_files=[x for x in self.all_files if x.endswith(IMG_EXTENSIONS)]
-        self.all_classes=[path_to_class(i) for i in self.dataset_files]
-        self.input_data=pd.DataFrame(list(zip(self.dataset_files, self.all_classes)), columns=[self.image_path_column, self.image_label_column])
-        if len(self.dataset_files)==0:
-            print ('Error! No data files found in directory:', self.data_directory)
-        if len(self.classes)==0:
-            print ('Error! No classes extracted from directory:', self.data_directory)
-
-
 def load_predefined_datatables(*args, **kwargs):
     train_dataset=Dataset_from_table(**kwargs, table=kwargs['predefined_datasets']['train'])
     valid_dataset=Dataset_from_table(**kwargs, table=kwargs['predefined_datasets']['valid'])
@@ -174,6 +58,32 @@ def calculate_mean_std(dataloader):
     mean /= nb_samples
     std /= nb_samples
     return (mean, std)
+
+
+def balance_dataset(dataset, label_col, upsample=True):
+  balanced_dataset=copy.deepcopy(dataset)
+  df = balanced_dataset.input_data
+  counts=df.groupby(label_col).count()
+  classes=df[label_col].unique().tolist()
+  max_class_num=counts.max()[0]
+  max_class_id=counts.idxmax()[0]
+  min_class_num=counts.min()[0]
+  min_class_id=counts.idxmin()[0]
+  if upsample:
+    resampled_subsets = [df[df[label_col]==max_class_id]]
+    for i in [x for x in classes if x != max_class_id]:
+      class_subset=df[df[label_col]==i]
+      upsampled_subset=resample(class_subset, n_samples=max_class_num, random_state=100)
+      resampled_subsets.append(upsampled_subset)
+  else:
+    resampled_subsets = [df[df[label_col]==min_class_id]]
+    for i in [x for x in classes if x != min_class_id]:
+      class_subset=df[df[label_col]==i]
+      upsampled_subset=resample(class_subset, n_samples=min_class_num, random_state=100)
+      resampled_subsets.append(upsampled_subset)
+  resampled_df = pd.concat(resampled_subsets)
+  balanced_dataset.input_data=resampled_df
+  return balanced_dataset
 
 
 def split_dataset(dataset, valid_percent=0.2, test_percent=0.2, equal_class_split=True, shuffle=True, sample=False,  **kwargs):
