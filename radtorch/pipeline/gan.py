210c6dea0190161404665d114a271779db610157
elbanan <elbanan@users.noreply.github.com>
2020-05-12 22:26:50 -0400

    Update gan.py

--- radtorch/pipeline/gan.py
+++ radtorch/pipeline/gan.py
@@ -110,7 +110,7 @@ class GAN():
                         transforms.transforms.Grayscale(self.d_input_image_channels),
                         transforms.ToTensor(),
                         transforms.Normalize(mean=self.normalize[0], std=self.normalize[1])])
-            elif d_input_image_channels != 3:
+            elif self.d_input_image_channels != 3:
                 self.transformations=transforms.Compose([
                     transforms.Resize((self.d_input_image_size, self.d_input_image_size)),
                     transforms.transforms.Grayscale(self.d_input_image_channels),
