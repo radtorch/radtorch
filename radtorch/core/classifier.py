1c423962d2a12d343381bd32e4990e1706b18402
elbanan <elbanan@users.noreply.github.com>
2020-05-16 22:59:31 -0400

    Update classifier.py

--- radtorch/core/classifier.py
+++ radtorch/core/classifier.py
@@ -317,7 +317,7 @@ class Classifier(object):
                 pass
         else:
             if self.type=='xgboost':
-                prediction=self.classifier.predict(image_features.iloc[0].to_numpy())
+                prediction=self.classifier.predict(image_features.to_numpy())
             else:
                 prediction=self.classifier.predict(image_features)
 
