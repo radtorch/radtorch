69e1150e7c96fce0b8a86429bec9fc03028ef1af
Mohamed Elbanan <elbanan@users.noreply.github.com>
2020-05-27 18:17:28 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -15,9 +15,9 @@ setup(
       install_requires=['torch', 'torchvision', 'torchsummary', 'numpy', 'pandas', 'pydicom', 'matplotlib', 'pillow', 
                         'tqdm', 'sklearn','pathlib', 'bokeh', 'xgboost', 'seaborn', 'torchsummary', 'efficientnet_pytorch', 
                         'xmltodict',
-                        'detectron2 @ git+https://github.com/facebookresearch/detectron2.git#egg=v0.1.3'
+                        'detectron2 @ git+https://github.com/facebookresearch/detectron2.git#egg=v0.1.3',
+                        'pyyaml>=5.1'
                        ],
-#       dependency_links=['http://github.com/facebookresearch/detectron2/tarball/master#egg=v0.1.3'],
 
       zip_safe=False,
       classifiers=[
