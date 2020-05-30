fa009989fbf2e22d2b485e8da3806bf696e8fb7f
elbanan <elbanan@users.noreply.github.com>
2020-05-30 12:12:34 -0400

    update for merge

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
       name='radtorch',
-      version='1.1.2',
+      version='1.1.4',
       version_date='5.14.2020',
       description='RADTorch, The Radiology Machine Learning Framework',
       url='https://radtorch.github.io/radtorch/',
@@ -12,8 +12,8 @@ setup(
       author_email = "https://www.linkedin.com/in/mohamedelbanan/",
       license='MIT',
       packages=find_packages(),
-      install_requires=['torch', 'torchvision', 'torchsummary', 'numpy', 'pandas', 'pydicom', 'matplotlib', 'pillow', 
-                        'tqdm', 'sklearn','pathlib', 'bokeh', 'xgboost', 'seaborn', 'torchsummary', 'efficientnet_pytorch', 
+      install_requires=['torch', 'torchvision', 'torchsummary', 'numpy', 'pandas', 'pydicom', 'matplotlib', 'pillow',
+                        'tqdm', 'sklearn','pathlib', 'bokeh', 'xgboost', 'seaborn', 'torchsummary', 'efficientnet_pytorch',
                         'xmltodict',
 #                         'detectron2 @ git+https://github.com/facebookresearch/detectron2.git#egg=v0.1.3',
 #                         'pyyaml>=5.1'
